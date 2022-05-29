b92b089ac417b8475ce518c9403895e582f632af
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-05-29 11:49:08 +0100

    render cities js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 // Global Declarations
 // my api key
 const API_KEY = "12d95f7e12mshd5a9c017ef2a138p13e781jsn9da0c1e65010";
+const recentCitiesContainer = $("#recent-cities");
 
 const options = {
   method: "GET",
@@ -59,10 +60,43 @@ const writeToLocalStorage = (key, value) => {
 };
 
 const renderCities = () => {
-  // get recent cities from LS []
-  // if [] is empty then render alert
-  // else render all recent cities
+  // get recent cities from LS
+  const recentCities = readFromLocalStorage("recentCities", []);
+  console.log("ready");
+
+  if (!recentCities.length) {
+    const alert = `<div class="alert alert-primary d-flex align-items-center" id ="no-cities-alert"role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+              <div>
+    There are no recent explores of cities
+    </div>
+  </div>`;
+    recentCitiesContainer.append(alert);
+    // if empty show alert
+  } else {
+    const createRecentSearchCity = (city) => {
+      return `<button type="button" class="btn btn-secondary btn-lg btn-block my-1" data-city="${city}">
+          ${city}
+          </button>`;
+    };
+    const recentSearches = recentCities.map(createRecentSearchCity).join("");
+    // render recent cities
+    recentCitiesContainer.append(recentSearches);
+  }
+
   // add an event listener on div containing all cities
+};
+
+const handleRecentSearchClick = (event) => {
+  // restrict click to only buttons
+  const target = $(event.target);
+  if (target.is("button")) {
+    // get data-city attribute
+    const cityName = target.attr("data-city");
+    console.log(cityName);
+  }
 };
 
 const renderCurrentWeather = (currentWeatherData) => {
@@ -93,8 +127,9 @@ const handleFormSubmit = () => {
   // else render weather data
 };
 
+recentCitiesContainer.click(handleRecentSearchClick);
 const onReady = () => {
-  // render recent cities
+  renderCities();
 };
 
 $(document).ready(onReady);
